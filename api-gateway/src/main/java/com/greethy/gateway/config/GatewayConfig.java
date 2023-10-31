cb6e561ad810d6b6f744b524623c5acc2ad5bac6
ThanhKien00 <kien.nt112002@gmail.com>
2023-10-31 17:50:36 +0700

    setup microservice's component

--- api-gateway/src/main/java/com/greethy/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/greethy/gateway/config/GatewayConfig.java
@@ -1,7 +1,23 @@
 package com.greethy.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ *
+ * @author ThanhKien
+ * */
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(predicate -> predicate.path("/api/v*/**")
+                        .uri("http://localhost:8085"))
+                .build();
+    }
+
 }
